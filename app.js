6f08193f366c2f256183683222972b59d6166f50
afquidangen <alyssafayequidangen@gmail.com>
2024-09-06 21:29:30 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+//ADD
+const favNumber1 = 14;
+const favNumber2 = 12;
+const sum = favNumber1 + favNumber2;
+console.log(sum);
+
+//SUB
+let Number1 = 14;
+let Number2 = 12;
+let difference = Number1 - Number2;
+console.log(difference);
+
+//MUL
+const favNum1 = 14;
+const favNum2 = 12;
+const product = favNum1 * favNum2;
+console.log(product);
+
+//DIV
+let Num1 = 14;
+let Num2 = 2;
+let quotient = Num1 / Num2;
+console.log(quotient);
+
+
